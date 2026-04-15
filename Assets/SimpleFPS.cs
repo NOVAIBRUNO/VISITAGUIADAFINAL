a15c5898291850412c2958d0dc706d418857d963
NOVAIBRUNO <brunowescley145@gmail.com>
2026-04-15 10:21:21 -0300

    atualizacao do projeto

---
+++ Assets/SimpleFPS.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class SimpleFPS : MonoBehaviour
+{
+    public float speed = 5f;
+    public float mouseSensitivity = 2f;
+    public float flySpeed = 5f;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // MOUSE
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * 100f * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * 100f * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.Rotate(0f, mouseX, 0f);
+
+        if (Camera.main != null)
+        {
+            Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        }
+
+        // MOVIMENTO
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.forward * vertical + transform.right * horizontal;
+
+        // SUBIR/DESCER
+        if (Input.GetKey(KeyCode.Space))
+            move.y += flySpeed;
+
+        if (Input.GetKey(KeyCode.LeftControl))
+            move.y -= flySpeed;
+
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
